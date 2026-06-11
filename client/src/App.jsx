112356cb41cc5ded3eeee165355578745974a04a
jatin-j <infinitywastaken80808080@gmail.com>
2026-06-12 03:23:58 +0530

    add cd 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -69,7 +69,7 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Todo App !!</h1>
+      <h1>Todo App deployed on EC2!</h1>
 
       <form onSubmit={addTodo} className="todo-form">
         <input
